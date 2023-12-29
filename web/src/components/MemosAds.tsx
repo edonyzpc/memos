ec9d106447a89d6ff2430535b2fc9b4b446ad134
edonyzpc <edonyzpc@yahoo.com>
2023-12-29 22:48:00 +0800

    fix: adsense does not been triggered
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- web/src/components/MemosAds.tsx
+++ web/src/components/MemosAds.tsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
+
+declare global {
+  interface Window {
+    // ⚠️ notice that "Window" is capitalized here
+    adsbygoogle: any;
+  }
+}
 
 const MemosAds = () => {
-  const adSenseCode = `
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1642118466411022" crossorigin="anonymous"></script>
-    <!-- Memos Ads -->
-    <ins class="adsbygoogle"
-      style="display:flex;align-items: center;"
-      data-ad-client="ca-pub-1642118466411022"
-      data-ad-slot="9206857864"
-      data-ad-format="auto"
-      data-full-width-responsive="true"></ins>
-    <script>
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-    `;
+  useEffect(() => {
+    try {
+      (window.adsbygoogle = window.adsbygoogle || []).push({});
+    } catch (e) {
+      console.error(e);
+    }
+  }, []);
 
   return (
-    <div
-      className="w-full px-1 py-2 flex flex-col justify-start items-start shrink-0 space-y-2"
-      dangerouslySetInnerHTML={{ __html: adSenseCode }}
-    />
+    <>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "flex" }}
+        data-ad-client="ca-pub-1642118466411022"
+        data-ad-slot="9206857864"
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+    </>
   );
 };
 
