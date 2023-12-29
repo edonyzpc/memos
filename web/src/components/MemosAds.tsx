cf1bc801015c92c113fd7b80923e16f1482b5dae
edonyzpc <edonyzpc@yahoo.com>
2023-12-29 11:50:57 +0800

    feat: memos service supporting adsense
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

---
+++ web/src/components/MemosAds.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MemosAds = () => {
+  const adSenseCode = `
+    <!-- Memos Ads -->
+    <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-1642118466411022"
+      data-ad-slot="9206857864"
+      data-ad-format="auto"
+      data-full-width-responsive="true"></ins>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+    `;
+
+  return <div className="w-full px-1 py-2 flex flex-col justify-start items-start shrink-0 space-y-2" dangerouslySetInnerHTML={{ __html: adSenseCode }} />;
+};
+
+export default MemosAds;
