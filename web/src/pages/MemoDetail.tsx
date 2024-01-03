5ac74d0917bc5387f77e083aa5f01602cc382371
edonyzpc <edonyzpc@yahoo.com>
2024-01-03 15:21:47 +0800

    fix: adsbygoogle.push() error: No slot size for availableWidth=0
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- web/src/pages/MemoDetail.tsx
+++ web/src/pages/MemoDetail.tsx
@@ -210,7 +210,9 @@ const MemoDetail = () => {
             {currentUser && (
               <MemoEditor key={memo.id} cacheKey={`comment-editor-${memo.id}`} parentMemoId={memo.id} onConfirm={handleCommentCreated} />
             )}
-            <MemosAds dataAdSlot="4954151033" />
+            <div className="max-w-xs w-full flex flex-col justify-center items-center py-6 mb-2">
+              <MemosAds dataAdSlot="4954151033" />
+            </div>
           </div>
         </div>
       </div>
