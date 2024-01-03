9722f590efb51335bb6aef2cda90661fec1a1343
edonyzpc <edonyzpc@yahoo.com>
2024-01-03 22:48:52 +0800

    fix: remove flex attribute
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- web/src/layouts/Root.tsx
+++ web/src/layouts/Root.tsx
@@ -14,7 +14,7 @@ function Root() {
         {sm && (
           <div className="hidden sm:block fixed top-0 left-0 w-56 md:w-64 px-4 border-r dark:border-zinc-800 h-full bg-zinc-50 dark:bg-zinc-700 dark:bg-opacity-40 transition-all hover:shadow-xl z-2">
             <Navigation />
-            <div className="w-full h-full overflow-auto flex flex-col justify-start items-start py-4 md:pt-6 z-30">
+            <div className="w-full h-full overflow-auto justify-start items-start py-4 md:pt-6 z-30">
               <MemosAds dataAdSlot="9206857864" />
             </div>
           </div>
