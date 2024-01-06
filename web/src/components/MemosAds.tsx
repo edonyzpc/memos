421ebf9dd4b2cebdfaa23bd65f805340b14226e7
edonyzpc <edonyzpc@yahoo.com>
2024-01-06 22:18:46 +0800

    feat: add multiplex ads
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- web/src/components/MemosAds.tsx
+++ web/src/components/MemosAds.tsx
@@ -28,6 +28,16 @@ const MemosAds = (props: any) => {
         data-ad-format="auto"
         data-full-width-responsive="true"
       ></ins>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "flex" }}
+        data-matched-content-rows-num="2,2"
+        data-matched-content-columns-num="1,2"
+        data-matched-content-ui-type="image_stacked,image_stacked"
+        data-ad-format="autorelaxed"
+        data-ad-client="ca-pub-1642118466411022"
+        data-ad-slot="7148428595"
+      ></ins>
     </>
   );
 };
