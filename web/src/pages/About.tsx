16951671d977adb861124414efed75f18b481146
edonyzpc <edonyzpc@yahoo.com>
2024-01-18 20:26:35 +0800

    feat: add edony customized info in about page
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -1,8 +1,14 @@
 import { Link } from "@mui/joy";
 import Icon from "@/components/Icon";
 import MobileHeader from "@/components/MobileHeader";
+import { useGlobalStore } from "@/store/module";
+import { edonyVersion } from "@/utils/version";
 
 const About = () => {
+  const globalStore = useGlobalStore();
+  const profile = globalStore.state.systemStatus.profile;
+  const customizedInfo = edonyVersion;
+
   return (
     <section className="@container w-full max-w-5xl min-h-full flex flex-col justify-start items-center sm:pt-3 md:pt-6 pb-8">
       <MobileHeader />
@@ -29,6 +35,21 @@ const About = () => {
               Documents
             </Link>
           </div>
+        </div>
+        <div className="w-full flex flex-row justify-start items-center text-sm italic">
+          <a className="shrink-0 flex flex-row justify-start items-center mx-1 hover:underline" href="https://usememos.com" target="_blank">
+            <img className="w-auto h-7" src="https://www.usememos.com/full-logo-landscape.png" alt="" />
+          </a>
+          <span>v{profile.version}</span>
+          <a
+            href="https://github.com/edonyzpc/memos/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex ml-6 justify-end text-sm hover:underline italic text-red-500"
+          >
+            <img className="w-auto h-7" src="https://img.edony.ink/1573133907wings-cricut-freesvg.org.shadow.walker.edit.svg" alt="" />
+            <span className="mx-4 my-auto">{customizedInfo}</span>
+          </a>
         </div>
       </div>
     </section>
--- web/src/utils/version.ts
+++ web/src/utils/version.ts
@@ -1,4 +1,4 @@
 // edonyversion is used to define the customized version from edonyzpc/memos.
-const Version = "1.11.7";
+const Version = "1.11.8";
 const Name = "@edonyzpc";
 export const edonyVersion = Name + " " + Version;
