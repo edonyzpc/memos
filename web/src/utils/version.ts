05a4f76dc2d8ab4d167a058518f845bdf9cc5d2c
edonyzpc <edonyzpc@yahoo.com>
2024-07-29 20:28:02 +0800

    feat: keep customized features after upgrade v0.22.4
    - share memos as images
    - keep release github action in release branch
    
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- web/src/utils/version.ts
+++ web/src/utils/version.ts
@@ -1,4 +1,4 @@
 // edonyversion is used to define the customized version from edonyzpc/memos.
-const Version = "1.12.6";
+const Version = "1.12.7";
 const Name = "Memos By Edony";
 export const edonyVersion = Name + "(v" + Version + ")";
