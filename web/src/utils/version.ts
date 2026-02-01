143b4f60e4d409f29ebc285cccef5326467dbe0d
edonyzpc <edonyzpc@yahoo.com>
2026-02-01 21:50:42 +0800

    release customized memos to version 2.1.2
    Signed-off-by: edonyzpc <edonyzpc@yahoo.com>

--- web/src/utils/version.ts
+++ web/src/utils/version.ts
@@ -1,4 +1,4 @@
 // edonyversion is used to define the customized version from edonyzpc/memos.
-const Version = "2.1.1";
+const Version = "2.1.2";
 const Name = "Memos By Edony";
 export const edonyVersion = Name + "(v" + Version + ")";
